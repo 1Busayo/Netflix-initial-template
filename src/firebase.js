e4c6263b0dd36a06247d3a737145fe1307143e36
unknown <fanimokunbusayo@gmail.com>
2020-12-11 20:49:51 +0100

    preparing netflix template

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBHrW1QwtkqV-Ns4D_UrSjb7Y7j3cNSwWw",
+    authDomain: "netflix-clone-959d3.firebaseapp.com",
+    projectId: "netflix-clone-959d3",
+    storageBucket: "netflix-clone-959d3.appspot.com",
+    messagingSenderId: "410611923356",
+    appId: "1:410611923356:web:f9b883425f7f8cbdfbb059"
+  };
